3df68b1ad019324fe3c9f771f5a3cd1c093eea2b
Daniel Juncos <daniel.juncos.01@gmail.com>
2017-02-13 11:41:31 -0500

    first commit

---
+++ js/js.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	
+
+	setTimeout(function(){
+		$(".icon").css("visibility", "visible")
+}, 5000);
+  	
+})
+
+function showhide1(id) {
+       	var e = document.getElementById(id);
+       	e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+     }
+
+function showhide2(id) {
+       	var e = document.getElementById(id);
+       	e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+     }
+
+function showhide3(id) {
+       	var e = document.getElementById(id);
+       	e.style.display = (e.style.display == 'block') ? 'none' : 'block';
+     }
